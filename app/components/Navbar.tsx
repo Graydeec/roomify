1dac84ba5fe7df9dc055555e6c7284c304eb2a1f
GrayDee <43388632+GrayDee@users.noreply.github.com>
2026-03-04 19:16:24 -0500

    01: Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import { Box } from "lucide-react";
+import React from "react";
+import Button from "./ui/Button";
+
+const Navbar = () => {
+  const isSignedIn = true; // Replace with actual authentication logic
+  const username = "John Doe"; // Replace with actual username from authentication
+  const handleAuthClick = async () => {};
+
+  return (
+    <header className="navbar">
+      <nav className="inner">
+        <div className="left">
+          <div className="brand">
+            <Box className="logo" />
+            <span className="name">Roomify</span>
+          </div>
+          <ul className="links">
+            <a href="#">Product</a>
+            <a href="#">Pricing</a>
+            <a href="#">Community</a>
+            <a href="#">Enterprise</a>
+          </ul>
+        </div>
+        <div className="actions">
+          {isSignedIn ? (
+            <>
+              <span className="greeting">
+                {username ? `Hi, ${username}` : "Signed in"}
+              </span>
+              <Button size="sm" onClick={handleAuthClick} className="btn">
+                Log Out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                onClick={handleAuthClick}
+                className="login"
+                size="sm"
+                variant="ghost"
+              >
+                Log In
+              </Button>
+              <a href="#upload" className="cta">
+                Get Started
+              </a>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
